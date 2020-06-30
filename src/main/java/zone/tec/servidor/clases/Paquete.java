4bff88a58efb6f7f6f53a0bbb1b2a185a79401a3
Adrian <adriantec2019@gmail.com>
2020-06-29 20:20:45 -0600

    Servidor
    Corrección de errores de configuración serios

---
+++ src/main/java/zone/tec/servidor/clases/Paquete.java
@@ -0,0 +1,63 @@
+package zone.tec.servidor.clases;
+
+
+import java.io.Serializable;
+
+/* Paquete class makes packages for data flows
+ * @author Adrian Gonzalez Jimenez
+ * @Version 04/03/2020
+ *
+ */
+public class Paquete implements Serializable //para enviar el paquete tengo que convertirlo en una serie de bites
+{
+    private String Nombre;
+    private String ip;
+    private String mensaje;
+    private Integer port;
+    private String remitente;
+
+    //SETTERS AND GETTERS
+    public String getRemitente() {
+        return remitente;
+    }
+    public void setRemitente(String remitente) {
+        this.remitente = remitente;
+    }
+
+    public Paquete(){
+        Nombre="";
+        ip="";
+        mensaje="";
+        port=9999;
+    }
+    public Integer getPort() { return port; }
+
+    public void setPort(Integer port) { this.port = port; }
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+
+
+
+}
