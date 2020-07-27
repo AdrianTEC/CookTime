ccf8af316844406ed9179102fef2649f6f18cf04
Adrian <adriantec2019@gmail.com>
2020-07-27 17:47:41 -0600

    arreglo

--- src/main/java/zone/tec/servidor/servlets/RecipesServlet.java
+++ src/main/java/zone/tec/servidor/servlets/RecipesServlet.java
@@ -148,7 +148,9 @@ public class RecipesServlet extends HttpServlet {
         if(req.getParameter("Target").equals("comentarios"))
             {
                 Receta receta= (Receta) arbolAVL.giveMebyID(req.getParameter("Id")).getElemento();
-            
+                Comentario nuevocomentario= new Comentario(req.getParameter("DATA"));
+                JSONObject comment= new JSONManager(getServletContext()).convertToJSON(nuevocomentario);
+                receta.getComentarios().add(comment);
 
 
 
